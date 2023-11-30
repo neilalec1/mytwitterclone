67816592ad7acfe196c4df6f4b0f5e920b2bd21d
neil <40416093@live.napier.ac.uk>
2023-11-30 20:15:55 +0000

    migration to PostgreSQL

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mytwitterclone_user:lk0jh11H8CqMysxKqCU8psoZYwIvyIMZ@dpg-clkdedmrem5c73ah2atg-a/mytwitterclone' #PostGreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mytwitterclone_user:lk0jh11H8CqMysxKqCU8psoZYwIvyIMZ@dpg-clkdedmrem5c73ah2atg-a.frankfurt-postgres.render.com/mytwitterclone' #PostGreSQL database
 app.config['SECRET_KEY'] = '\xe9d\xc1\xaf\xe6\x8c\x90\xcc\xba\x9f>\xc0\x19\xb1k\x15\x07\x14(\xf0\x7fQ&\xaa'  #my not so secret key
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
